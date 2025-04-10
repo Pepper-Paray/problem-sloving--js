cad618261be9cfd742553f931ee6fa7b0b051180
Pepper <pandapup3000@gmail.com>
2025-04-10 13:01:53 -0500

    added java script

---
+++ script.js
@@ -0,0 +1,13 @@
+//Each guest gets 2 sandwiches. You have 12 guests.
+//How many sandwiches do you need in total?
+let guests;
+guests = 12;
+let sandwiches;
+sandwiches = 2;
+let Amount;
+Amount = guests * sandwiches;
+
+
+console.log(
+    Amount = guests * sandwiches
+)
