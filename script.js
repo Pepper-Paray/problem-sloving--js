fd2975ed9ab52a58c7bda5e03ab97dcc691ca43f
Pepper <pandapup3000@gmail.com>
2025-04-11 17:35:29 -0500

    added final touch

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
 //Each guest gets 2 sandwiches. You have 12 guests.
 //How many sandwiches do you need in total?
-let guests;
-guests = 12;
-let sandwiches;
-sandwiches = 2;
+Guest = 12
+sandwiches = 2
+needed = (12 * 2)
 console.log(
-    "guests:", guests, "sandwiches:", sandwiches
+    "Guest:", Guest, "sandwiches", sandwiches, "needed:", needed
 )
-let Amount;
-Amount = guests * sandwiches;
+//You have $40. A movie ticket costs $9.50.
+//How many full tickets can you buy, and how much money is left over?
+//(Hint: use Math.floor() and %)
+amount = 40
+ticket = 9.50
+Math.floor % (40 / 9.50);
+moneyLeft = Math.round(amount / ticket)
+console.log("amount:", amount, "ticket:", ticket, "moneyLeft:", moneyLeft)
+//Convert a dog’s age to human years (1 dog year = 7 human years).
+function humanYearsToDogYears(humanYears) {
+    // The first two years of a dog's life count as 10.5 dog years each
+    // After that, each human year counts as 4 dog years
+    if (humanYears <= 2) {
+        return humanYears * 10.5;
+    } else {
+        return 21 + (humanYears - 2) * 4;
+    }
+}
 
+let humanYears = 5;
+let dogYears = humanYearsToDogYears(humanYears);
+console.log(`${humanYears} human years is approximately ${dogYears} dog years.`);
+//Create 3 variables:
 
-console.log(
-    " Amount:"= guests * sandwiches 
-)
+//• firstName
+
+//• favoriteFood
+
+//• hobby
+
+//Print a sentence like:
+
+//“Hi, my name is Alex. I love tacos and playing guitar.”
+let firstName="Bug"
+let favoriteFood="dumplings"
+let hobby="models"
+let sentence = `Hello, my name is ${firstName} and I love ${favoriteFood} and ${hobby}.`
+console.log(sentence)
